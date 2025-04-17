6b6bd49918ff00d7efda5f679a1d2173c5246076
karkeeg <karkibibek642@gmail.com>
2025-04-17 22:28:59 +0545

    Header and Hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+    </mesh>
+  );
+};
 
-export default Computers
+export default Computers;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ module.exports = {
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        violetPrimary: "#7e22ce",
+        violetSecondary: "#a855f7",
       },
+
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
@@ -19,7 +22,11 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "violet-glow": "linear-gradient(135deg, #7e22ce 0%, #7e22ce80 100%)",
+
+        "violet-gradient": "linear-gradient(90deg, #7e22ce, #a855f7)",
+
+        "hero-pattern": "url('/src/assets/image.png')",
       },
     },
   },
